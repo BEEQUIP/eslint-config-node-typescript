d1489a258c919bbd3446d2c4ccad55728d035386
Robert van Kempen <DoNormal@users.noreply.github.com>
2020-10-22 13:59:10 +0200

    fix: update packages to support ES syntax

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    'extends': [
+    extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
         'plugin:jest/recommended',
-        "plugin:node/recommended"
+        'plugin:node/recommended',
     ],
-    'plugins': ['@typescript-eslint', 'prettier', 'jest'],
+    plugins: ['@typescript-eslint', 'prettier', 'jest'],
     parserOptions: {
-        "ecmaVersion": 2020
+        ecmaVersion: 2020,
     },
-    'rules': {
+    rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error', {
             'vars': 'all',
@@ -20,17 +20,25 @@ module.exports = {
             'ignoreRestSiblings': false,
         }],
     },
-    'overrides': [
+    overrides: [
         {
-            'files': ['*.js'],
-            'rules': {
+            files: ['*.js'],
+            rules: {
                 '@typescript-eslint/no-var-requires': 'off',
             },
         },
+        {
+            files: ['*.ts'],
+            rules: {
+                // Disable reporting on ES syntax (e.g. import statements),
+                // since we’re always compiling to ES 5 with TypeScript
+                'node/no-unsupported-features/es-syntax': 'off',
+            }
+        }
     ],
-    "settings": {
-        "node": {
-            "tryExtensions": [".ts", ".js", ".json"]
+    settings: {
+        node: {
+            tryExtensions: ['.ts', '.js', '.json'],
         }
     },
 };
